4f2d3c5aad69779327d1673ad759af9934f0e49c
Fauste1 <stepan.fau@gmail.com>
2021-07-04 20:26:18 +0200

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Meal prep app</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // this will hold components
   }
 }
 </script>
